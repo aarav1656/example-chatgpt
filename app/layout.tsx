571a00146775dffff3400ac000dfa54207ac6be0
Aarav raj <anshuk3917@gmail.com>
2024-07-07 13:56:02 +0530

    update to page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-import {Analytics} from '@vercel/analytics/react'
-import {IBM_Plex_Mono} from 'next/font/google'
-import {Toaster} from 'react-hot-toast'
-import {Header} from '@/components/Header'
-import {Web3ModalProvider} from "@/context/Web3Modal";
+import { Analytics } from '@vercel/analytics/react'
+import { IBM_Plex_Mono } from 'next/font/google'
+import { Toaster } from 'react-hot-toast'
+import { Header } from '@/components/Header'
+// import { Mint } from '@/components/Mint'
+import { Web3ModalProvider } from '@/context/Web3Modal'
 import ThemesProvider from '@/providers/ThemesProvider'
 import '@/styles/globals.scss'
 import '@/styles/theme-config.css'
@@ -21,23 +22,20 @@ export const metadata = {
 }
 
 // const inter = Inter({subsets: ['latin'], variable: '--font-inter'})
-const plexmono = IBM_Plex_Mono(
-  {weight: "400", subsets: ["latin"]},
-);
+const plexmono = IBM_Plex_Mono({ weight: '400', subsets: ['latin'] })
 
-export default function RootLayout({children}: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-    <body className={plexmono.className}>
-    <ThemesProvider>
-      <Header/>
-      <Web3ModalProvider>
-        {children}
-      </Web3ModalProvider>
-      <Toaster/>
-    </ThemesProvider>
-    <Analytics/>
-    </body>
+      <body className={plexmono.className}>
+        <ThemesProvider>
+          <Header />
+          {/* <Mint /> */}
+          <Web3ModalProvider>{children}</Web3ModalProvider>
+          <Toaster />
+        </ThemesProvider>
+        <Analytics />
+      </body>
     </html>
   )
 }
